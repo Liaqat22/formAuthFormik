34931c0ef980dd01a6490c79b95f7cb521612613
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-10-31 18:14:37 +0500

    Delete src directory

--- src/pages/APIs.js
+++
@@ -1,6 +0,0 @@
-
-const fetchingProducts = 'https://dummyjson.com/products'
-const addProduct = 'https://dummyjson.com/products/add'
-const searchproducts = 'https://dummyjson.com/products/search?q='
-
-export {fetchingProducts , addProduct ,searchproducts}
